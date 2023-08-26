fe54399f59d2e700d43263d6698d3a45f0e82c3f
Ramez Nasser <97125244+RamezNasser5@users.noreply.github.com>
2023-08-26 21:58:05 +0300

    Update color_view.dart
    change colors of my tune app

--- lib/screens/color_view.dart
+++ lib/screens/color_view.dart
@@ -5,21 +5,20 @@ import 'package:tune_player_app/view_items/items.dart';
 class ColorToneView extends StatelessWidget {
   const ColorToneView({super.key});
   final List<BlockModel> blocks = const [
-    BlockModel(color: 0xFFF44336, sound: 'note1.wav'),
-    BlockModel(color: 0xFFFF9800, sound: 'note2.wav'),
-    BlockModel(color: 0xFFE5FF00, sound: 'note3.wav'),
-    BlockModel(color: 0xFF00FF22, sound: 'note4.wav'),
-    BlockModel(color: 0xFF7700FF, sound: 'note5.wav'),
-    BlockModel(color: 0xFF00FFFF, sound: 'note6.wav'),
-    BlockModel(color: 0xFFEA00FF, sound: 'note7.wav'),
+    BlockModel(color: 0xFF333333, sound: 'note1.wav'),
+    BlockModel(color: 0xFF444444, sound: 'note2.wav'),
+    BlockModel(color: 0xFF555555, sound: 'note3.wav'),
+    BlockModel(color: 0xFF666666, sound: 'note4.wav'),
+    BlockModel(color: 0xFF777777, sound: 'note5.wav'),
+    BlockModel(color: 0xFF888888, sound: 'note6.wav'),
+    BlockModel(color: 0xFF999999, sound: 'note7.wav'),
   ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFFEF6DB),
       appBar: AppBar(
-        title: const Text("Play Tone"),
-        backgroundColor: const Color(0xFF46322B),
+        title: const Center(child: Text("Play Tone")),
+        backgroundColor: Colors.transparent,
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
